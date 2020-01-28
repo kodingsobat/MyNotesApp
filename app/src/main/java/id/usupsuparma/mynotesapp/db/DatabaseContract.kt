990147a3a607a3cf00c725d17764ae4aba5d58dd
usupsuparma <usup.suparma7@gmail.com>
2020-01-28 13:05:09 +0700

    add service provider

--- app/src/main/java/id/usupsuparma/mynotesapp/db/DatabaseContract.kt
+++ app/src/main/java/id/usupsuparma/mynotesapp/db/DatabaseContract.kt
@@ -1,15 +1,28 @@
 package id.usupsuparma.mynotesapp.db
 
+import android.net.Uri
 import android.provider.BaseColumns
 
-internal class DatabaseContract {
-    internal class NoteColumns : BaseColumns {
+object DatabaseContract {
+
+    const val AUTHORITY = "id.usupsuparma.mynoteapp"
+    const val SCHEME = "content"
+
+    class NoteColumns : BaseColumns {
+
         companion object {
             const val TABLE_NAME = "note"
             const val _ID = "_id"
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
+
+            // untuk membuat URI content://com.dicoding.picodiploma.mynotesapp/note
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
         }
+
     }
 }
